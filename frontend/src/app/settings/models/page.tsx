103593a25cb2b807cf3af92b974c66d122ebe41d
Hugh Lyu <hugh.lv@gmail.com>
2025-01-31 00:29:21 +0800

    chore: models page

--- frontend/src/app/settings/models/page.tsx
+++ frontend/src/app/settings/models/page.tsx
@@ -7,17 +7,16 @@ import { Icons } from '@/components/icons';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { Collapsible } from '@/components/flow/option/collapsible';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Textarea } from '@/components/ui/textarea';
+import { useDebounce } from '@/hooks/use-debounce';
+import { Collapsible } from '@/components/ui/collapsible';
+import {
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@radix-ui/react-collapsible';
 
-const ModelForm = ({
-  model: sourceModel,
-  onSave,
-  onDelete,
-  isSaving,
-  isDeleting,
-}: any) => {
+const ModelForm = ({ model: sourceModel, onSave }: any) => {
   const [model, setModel] = useState('');
   const [description, setDescription] = useState('');
   const [apiKey, setApiKey] = useState('');
@@ -37,110 +36,96 @@ const ModelForm = ({
     setTags(sourceModel.tags);
   }, [sourceModel]);
 
-  const handleSave = () => {
+  const debouncedSave = useDebounce((updatedData: any) => {
     onSave({
       ...sourceModel,
-      model,
-      description,
-      api_key: apiKey,
-      base_url: baseUrl,
-      api_type: apiType,
-      api_version: apiVersion,
-      tags,
+      ...updatedData,
     });
+  }, 500);
+
+  const handleFieldChange = (field: string, value: string) => {
+    const setters: any = {
+      model: setModel,
+      description: setDescription,
+      api_key: setApiKey,
+      base_url: setBaseUrl,
+      api_type: setApiType,
+      api_version: setApiVersion,
+      tags: setTags,
+    };
+    setters[field](value);
+    debouncedSave({ [field]: value });
   };
 
   return (
-    <div className="w-full">
+    <div className="flex flex-col gap-2 w-full">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <label className="flex flex-col gap-1">
           <span>Model Name</span>
           <Input
             value={model || ''}
-            onChange={(e) => setModel(e.target.value)}
+            onChange={(e) => handleFieldChange('model', e.target.value)}
           />
         </label>
         <label className="flex flex-col gap-1">
           <span>API Key</span>
           <Input
             value={apiKey || ''}
-            onChange={(e) => setApiKey(e.target.value)}
+            onChange={(e) => handleFieldChange('api_key', e.target.value)}
           />
         </label>
         <label className="flex flex-col gap-1 md:col-span-2">
           <span>Description</span>
           <Textarea
             value={description || ''}
-            onChange={(e) => setDescription(e.target.value)}
+            onChange={(e) => handleFieldChange('description', e.target.value)}
           />
         </label>
-        <label className="flex items-center gap-2 md:col-span-2">
-          <Collapsible
-            checked={showAdvanced}
-            onChange={() => setShowAdvanced(!showAdvanced)}
-          />
-          <span>Show Advanced Options</span>
-        </label>
-        {showAdvanced && (
-          <>
-            <label className="flex flex-col gap-1">
-              <span>Base URL</span>
-              <Input
-                value={baseUrl || ''}
-                onChange={(e) => setBaseUrl(e.target.value)}
-              />
-            </label>
-            <label className="flex flex-col gap-1">
-              <span>API Type (e.g., azure)</span>
-              <Input
-                value={apiType || ''}
-                onChange={(e) => setApiType(e.target.value)}
-              />
-            </label>
-            <label className="flex flex-col gap-1">
-              <span>API Version</span>
-              <Input
-                value={apiVersion || ''}
-                onChange={(e) => setApiVersion(e.target.value)}
-              />
-            </label>
-            <label className="flex flex-col gap-1">
-              <span>Tags (Use comma as separater)</span>
-              <Input
-                value={tags || ''}
-                onChange={(e) => setTags(e.target.value)}
-              />
-            </label>
-          </>
-        )}
       </div>
-      <div className="flex justify-end gap-4 mt-4">
-        <Button className="flex items-center gap-2" onClick={handleSave}>
-          {isSaving && <div className="loading loading-xs"></div>}
-          Save Model
-        </Button>
-        <Button
-          className="flex items-center gap-2"
-          variant="destructive"
-          onClick={onDelete}
-        >
-          {isDeleting && <div className="loading loading-xs"></div>}
-          Delete Model
-        </Button>
-      </div>
+      <Collapsible className="w-full">
+        <CollapsibleTrigger asChild>
+          <div className="flex items-center gap-2 py-2">
+            <span>Show Advanced Options</span>
+            <Icons.chevronsUpDown className="w-4 h-4" />
+          </div>
+        </CollapsibleTrigger>
+        <CollapsibleContent className="grid grid-cols-2 gap-4 w-full">
+          <label className="flex flex-col gap-1 w-full">
+            <span>Base URL</span>
+            <Input
+              value={baseUrl || ''}
+              onChange={(e) => handleFieldChange('base_url', e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col gap-1">
+            <span>API Type (e.g., azure)</span>
+            <Input
+              value={apiType || ''}
+              onChange={(e) => handleFieldChange('api_type', e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col gap-1">
+            <span>API Version</span>
+            <Input
+              value={apiVersion || ''}
+              onChange={(e) => handleFieldChange('api_version', e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col gap-1">
+            <span>Tags (Use comma as separater)</span>
+            <Input
+              value={tags || ''}
+              onChange={(e) => handleFieldChange('tags', e.target.value)}
+            />
+          </label>
+        </CollapsibleContent>
+      </Collapsible>{' '}
     </div>
   );
 };
 
 const ModelCard = ({ model, onSave, onDelete }: any) => {
-  const [isSaving, setIsSaving] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
-
-  const handleSave = async (updatedModel: LlmModel) => {
-    setIsSaving(true);
-    await onSave(updatedModel);
-    setIsSaving(false);
-  };
 
   const handleDelete = async () => {
     setIsDeleting(true);
@@ -149,21 +134,28 @@ const ModelCard = ({ model, onSave, onDelete }: any) => {
   };
 
   return (
-    <Card className="">
+    <Card className="w-full bg-background">
       <CardHeader>
-        <CardTitle className="flex items-center gap-2">
-          <Icons.brain className="w-5 h-5" />
-          <h1 className="font-bold">Model: {model.model}</h1>
+        <CardTitle className="flex items-center gap-2 w-full justify-between">
+          <div className="flex items-center gap-2">
+            <Icons.brain className="w-5 h-5" />
+            <h1 className="font-bold">Model: {model.model}</h1>
+          </div>
+          <div>
+            <Button
+              className="flex items-center gap-2 text-red-300 hover:text-red-500"
+              variant="ghost"
+              onClick={handleDelete}
+            >
+              {isDeleting && <Icons.spinner className="animate-spin size-4" />}
+              {!isDeleting && <Icons.trash className="w-4 h-4" />}
+              Delete
+            </Button>
+          </div>
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <ModelForm
-          model={model}
-          onSave={handleSave}
-          onDelete={handleDelete}
-          isSaving={isSaving}
-          isDeleting={isDeleting}
-        />
+        <ModelForm model={model} onSave={onSave} />
       </CardContent>
     </Card>
   );
@@ -217,7 +209,7 @@ const Page = () => {
 
   return (
     <ScrollArea className="relative flex flex-col w-full gap-2 text-sm p-4">
-      <div className="flex items-center justify-between">
+      <div className="flex items-end justify-between mb-4">
         <div className="flex flex-col gap-2">
           <h1 className="text-lg font-bold">Models</h1>
           <div>
@@ -227,7 +219,7 @@ const Page = () => {
         </div>
         <div className="flex items-center justify-end gap-2 p-2">
           <Button className="flex items-center gap-2" onClick={onAddModel}>
-            {!isCreating && <Icons.brain className="w-4 h-4" />}
+            {!isCreating && <Icons.add className="w-4 h-4" />}
             {isCreating && <Icons.spinner className="w-4 h-4 animate-spin" />}
             New Model
           </Button>
